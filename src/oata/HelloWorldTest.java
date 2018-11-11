187c4c489cbb3838cb9b090a7f76f1f8706037ac
Ramkumar Chinnasamy <36261095+cramkumar@users.noreply.github.com>
2018-11-11 22:12:23 +0530

    Update HelloWorldTest.java
    uploaded by ram

--- src/oata/HelloWorldTest.java
+++ src/oata/HelloWorldTest.java
@@ -1,16 +1,9 @@
 package oata;
-
 import org.junit.Test;
-
 import static org.junit.Assert.fail;
 
 public class HelloWorldTest {
     
-    @Test
-    public static void main(String[] args) {
-        System.out.println("Hello WorldTest");
-    }
-
     @Test
     public void testNothing() {
     }
